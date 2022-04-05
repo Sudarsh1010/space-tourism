0eef0d9bc7fd95583cf0697ce1a94d914f781039
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-04-05 21:24:52 +0530

    Update images.js
    added Destination page's Images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,18 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
-// Home Page Images
-import homeDeskBg from '../assets/home/background-home-desktop.jpg'
-import homeTabletBg from '../assets/home/background-home-tablet.jpg'
-import homeMobileBg from '../assets/home/background-home-mobile.jpg'
-
 // Shared
 import closeIcon from '../assets/shared/icon-close.svg'
 import hamburgerIcon from '../assets/shared/icon-hamburger.svg'
 import logo from '../assets/shared/logo.svg'
 
+// Destination
+import europa from '../assets/destination/image-europa.webp'
+import mars from '../assets/destination/image-mars.webp'
+import moon from '../assets/destination/image-moon.webp'
+import titan from '../assets/destination/image-titan.webp'
+
 export default {
-  homeDeskBg,
-  homeMobileBg,
-  homeTabletBg,
+  // destination
+  europa,
+  mars,
+  moon,
+  titan,
+
+  // Shared
   closeIcon,
   hamburgerIcon,
   logo,
