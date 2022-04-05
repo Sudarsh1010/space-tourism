43a02d6d04a6ede0e3dfd5bec098b0705f635d9c
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-04-05 10:13:18 +0530

    Home and Shared images exports

---
+++ src/constants/images.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+// Home Page Images
+import homeDeskBg from '../assets/home/background-home-desktop.png'
+import homeTabletBg from '../assets/home/background-home-tablet.png'
+import homeMobileBg from '../assets/home/background-home-mobile.png'
+
+// Shared
+import closeIcon from '../assets/shared/icon-close.svg'
+import hamburgerIcon from '../assets/shared/icon-hamburger.svg'
+import logo from '../assets/shared/logo.svg'
+
+export default {
+  homeDeskBg,
+  homeMobileBg,
+  homeTabletBg,
+  closeIcon,
+  hamburgerIcon,
+  logo,
+}
